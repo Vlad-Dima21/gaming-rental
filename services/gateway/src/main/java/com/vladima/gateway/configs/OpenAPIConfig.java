f6b9571490093dd8f195fa8b80c773605569cdf5
Andreea Creita <andreea_creita@yahoo.com>
2024-12-26 19:30:17 +0200

    configuration for non-working springdoc

---
+++ services/gateway/src/main/java/com/vladima/gateway/configs/OpenAPIConfig.java
@@ -0,0 +1,9 @@
+package com.vladima.gateway.configs;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+}

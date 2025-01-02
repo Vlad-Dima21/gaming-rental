70f80afeeb8c723df96b2dc0a6c6768ff88740f0
Vlad-Dima21 <vladima2001@gmail.com>
2025-01-02 16:59:14 +0200

    moved springdoc config to yml

--- services/client/src/main/java/com/vladima/gamingrental/configs/OpenAPIConfig.java
+++ services/client/src/main/java/com/vladima/gamingrental/configs/OpenAPIConfig.java
@@ -2,17 +2,28 @@ package com.vladima.gamingrental.configs;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
 
 @Configuration
 public class OpenAPIConfig {
 
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI openAPI(
+        @Value("${openapi.title}") String title,
+        @Value("${openapi.description}") String description,
+        @Value("${openapi.version}") String version,
+        @Value("${openapi.url}") String url
+    ) {
         return new OpenAPI()
-                .info(new Info().title("Client API")
-                        .description("Client API")
-                        .version("1.0"));
+                .servers(List.of(new Server().url(url)))
+                .info(new Info().title(title)
+                        .description(description)
+                        .version(version));
     }
 }
--- services/gateway/src/main/java/com/vladima/gateway/configs/OpenAPIConfig.java
+++ services/gateway/src/main/java/com/vladima/gateway/configs/OpenAPIConfig.java
@@ -1,9 +1,20 @@
 package com.vladima.gateway.configs;
 
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 public class OpenAPIConfig {
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/client-service/v3/api-docs").and().method(HttpMethod.GET).uri("lb://client-service"))
+                .route(r -> r.path("/product-service/v3/api-docs").and().method(HttpMethod.GET).uri("lb://product-service"))
+                .route(r -> r.path("/rental-service/v3/api-docs").and().method(HttpMethod.GET).uri("lb://rental-service"))
+                .build();
+    }
 }
